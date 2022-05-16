c9273cb8d4428edfc285565c38d09f7908b009c2
vRokkiS <56517492+vRokkiS@users.noreply.github.com>
2022-05-16 14:43:20 +0300

    Test AAI

--- random.js
+++ random.js
@@ -1,23 +1,27 @@
-function load() {
-  var sityData = JSON.parse(generator_type);
-  alert(generator_type[0]);
-}
-
-$(document).ready(function(){
-  $.getJSON("./" + generator_type, function(data){
-      console.log(data); // Prints: Harry
-      console.log(data[0]); // Prints: 14
-  }).fail(function(){
-      console.log("An error has occurred.");
-  });
-});
 
 // GENERATOR TYPE
 
-let generator_type_temp = document.location.pathname.split("/")[3];
+let generator_type_temp = document.location.pathname.split("/")[8];
 let generator_type = generator_type_temp.split(".")[0];
 
 console.log("это генератор - " + generator_type);
+
+console.log("../data/" + generator_type + ".json");
+
+var requestURL = "../data/" + generator_type + ".json";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+// $(document).ready(function(){
+//   $.getJSON("randomanime.json", function(data){
+//       console.log(data); // Prints: Harry
+//       console.log(data[0]); // Prints: 14
+//   }).fail(function(){
+//       console.log("An error has occurred.");
+//   });
+// });
 
 // GET JSON FILE
 
@@ -36,29 +40,29 @@ console.log("это генератор - " + generator_type);
 
 // GET RANDOM VALUE 
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
 
-// SYSTEM INFO
+// // SYSTEM INFO
 
-let random_number = getRandomInt(0, Object.keys(sityData).length);
-let json_info = sityData[random_number];
+// let random_number = getRandomInt(0, Object.keys(sityData).length);
+// let json_info = sityData[random_number];
 
-console.log(Object.keys(sityData).length + ' объектов в бд');
-console.log('случайное число ' + random_number);
+// console.log(Object.keys(sityData).length + ' объектов в бд');
+// console.log('случайное число ' + random_number);
 
-// UPDATE FUNCTION
+// // UPDATE FUNCTION
 
-function updateInfo() {
-  let random_number = getRandomInt(0, Object.keys(sityData).length);
-  let json_info = sityData[random_number];
-  document.getElementById('image').src = '../media/random/' + generator_type + '/' + `${json_info["ID"]}` + '.jpg';
-  document.getElementById('title').textContent = `${json_info["Title"]}`;
-  document.getElementById('title_description').textContent  = `${json_info["Title_desc"]}`;
-  document.getElementById('description').textContent = `${json_info["Description"]}`;
-}
+// function updateInfo() {
+//   let random_number = getRandomInt(0, Object.keys(sityData).length);
+//   let json_info = sityData[random_number];
+//   document.getElementById('image').src = '../media/random/' + generator_type + '/' + `${json_info["ID"]}` + '.jpg';
+//   document.getElementById('title').textContent = `${json_info["Title"]}`;
+//   document.getElementById('title_description').textContent  = `${json_info["Title_desc"]}`;
+//   document.getElementById('description').textContent = `${json_info["Description"]}`;
+// }
 
-updateInfo()
+// updateInfo()
