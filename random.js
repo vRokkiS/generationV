df751165584bd47932ee9bae56994e65799f7a03
vRokkiS <56517492+vRokkiS@users.noreply.github.com>
2022-05-16 21:03:44 +0300

    Test ABS

--- random.js
+++ random.js
@@ -6,26 +6,35 @@ let generator_type = generator_type_temp.split(".")[0];
 
 console.log("это генератор - " + generator_type);
 
-
 // GET JSON
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const requestURL = "../generators/data/" + generator_type + ".json";
 
 function sendRequest() {
-  
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', requestURL);
-
     xhr.responseType = 'json';
-    
     xhr.send();
     
-    return xhr.response;
+    let data = JSON.parse(xhr.response);
+
+    let random_number = getRandomInt(0, Object.keys(data).length);
+    let json_info = data[random_number];
+    document.getElementById('image').src = '../media/random/' + generator_type + '/' + `${json_info["ID"]}` + '.jpg';
+    document.getElementById('title').textContent = `${json_info["Title"]}`;
+    document.getElementById('title_description').textContent  = `${json_info["Title_desc"]}`;
+    document.getElementById('description').textContent = `${json_info["Description"]}`;
 }
 
 sendRequest();
 
-let data = sendRequest();
+// let data = sendRequest();
 
 // xhr.onload = () => {
 //   console.log(xhr.responseText);
@@ -33,29 +42,20 @@ let data = sendRequest();
 
 // GET RANDOM VALUE 
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+
 
 // SYSTEM INFO
 
-let random_number = getRandomInt(0, Object.keys(data).length);
-let json_info = data[random_number];
 
-console.log(Object.keys(data).length + ' объектов в бд');
-console.log('случайное число ' + random_number);
+// let json_info = data[random_number];
+
+// console.log(Object.keys(data).length + ' объектов в бд');
+// console.log('случайное число ' + random_number);
 
 // UPDATE FUNCTION
 
-function updateInfo() {
-  let random_number = getRandomInt(0, Object.keys(sityData).length);
-  let json_info = data[random_number];
-  document.getElementById('image').src = '../media/random/' + generator_type + '/' + `${json_info["ID"]}` + '.jpg';
-  document.getElementById('title').textContent = `${json_info["Title"]}`;
-  document.getElementById('title_description').textContent  = `${json_info["Title_desc"]}`;
-  document.getElementById('description').textContent = `${json_info["Description"]}`;
-}
+// function updateInfo() {
+  
+// }
 
 // updateInfo()
