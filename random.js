25ab780ab066d62479133e4803cf510797d799c3
vRokkiS <56517492+vRokkiS@users.noreply.github.com>
2022-05-17 21:03:15 +0300

    Fix random.js
    Finally fixed function of getting JSON file

--- random.js
+++ random.js
@@ -1,17 +1,19 @@
-
 // GENERATOR TYPE
 
 let generator_type_temp = document.location.pathname.split("/")[3];
 let generator_type = generator_type_temp.split(".")[0];
 
-console.log("это генератор - " + generator_type);
+console.log("TEST:// generator_type - " + generator_type);
 
-// GET JSON
+// RANDOM NUMBER
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+// GET JSON 
 
 const requestURL = "../generators/data/" + generator_type + ".json";
 
@@ -24,42 +26,16 @@ function sendRequest() {
     return JSON.parse(xhr.responseText);
 }
 
-// Object.keys(data). length
-
- 
-
 let data = sendRequest();
 
-let random_number = getRandomInt(0, 10);
+// RANDOM
 
-    let json_info = data[random_number];
-    document.getElementById('image').src = '../media/random/' + generator_type + '/' + `${json_info["ID"]}` + '.jpg';
-    document.getElementById('title').textContent = `${json_info["Title"]}`;
-    document.getElementById('title_description').textContent  = `${json_info["Title_desc"]}`;
-    document.getElementById('description').textContent = `${json_info["Description"]}`;
+function random() {
+  let random_number = getRandomInt(0, Object.keys(data).length);
 
-// let data = sendRequest();
-
-// xhr.onload = () => {
-//   console.log(xhr.responseText);
-// }
-
-// GET RANDOM VALUE 
-
-
-
-// SYSTEM INFO
-
-
-// let json_info = data[random_number];
-
-// console.log(Object.keys(data).length + ' объектов в бд');
-// console.log('случайное число ' + random_number);
-
-// UPDATE FUNCTION
-
-// function updateInfo() {
-  
-// }
-
-// updateInfo()
+  let json_info = data[random_number];
+  document.getElementById('image').src = '../media/random/' + generator_type + '/' + `${json_info["ID"]}` + '.jpg';
+  document.getElementById('title').textContent = `${json_info["Title"]}`;
+  document.getElementById('title_description').textContent  = `${json_info["Title_desc"]}`;
+  document.getElementById('description').textContent = `${json_info["Description"]}`;
+}
